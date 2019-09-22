4f98aa9e47450798cbe7bbe61ce34b3660948621
Sebas <sebaze@gmail.comasdf>
2019-09-21 23:47:24 -0300

    putting jasper file in .properties. Not found error handler

--- src/main/java/ar/com/bambu/controlers/ReporteCotizacion.java
+++ src/main/java/ar/com/bambu/controlers/ReporteCotizacion.java
@@ -1,25 +1,28 @@
 package ar.com.bambu.controlers;
 
-import ar.com.bambu.entities.*;
+import ar.com.bambu.entities.EvCont;
+import ar.com.bambu.entities.EvMedios;
+import ar.com.bambu.entities.Eventos;
+import ar.com.bambu.entities.EventosId;
 import ar.com.bambu.models.Cotizacion;
 import ar.com.bambu.repos.EvContRepository;
 import ar.com.bambu.repos.EvMedioRepository;
 import ar.com.bambu.repos.EventosRepository;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import net.sf.jasperreports.engine.*;
-import net.sf.jasperreports.engine.export.*;
 
-import java.io.FileInputStream;
 import java.io.InputStream;
-import java.util.*;
-
+import java.util.HashMap;
 import java.util.List;
 
 
@@ -28,8 +31,8 @@ import java.util.List;
 public class ReporteCotizacion {
 
 
-    String fileName="Wood.jasper";
-
+    @Value("${jasper.file}")
+    String fileName;
 
     @Autowired
     EventosRepository repo;
@@ -38,11 +41,17 @@ public class ReporteCotizacion {
     @Autowired
     EvMedioRepository medioRepository;
 
-    @RequestMapping(path="/cotizacion", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_PDF_VALUE)
-    public ResponseEntity<ByteArrayResource>  getCotizacion(@RequestBody Eventos ev) throws Exception{
-        List<Eventos> all = repo.findAll();
-        Eventos evento = repo.findById(new EventosId(ev.getIdEvento(), ev.getCajaZ())).get();
-        List<EvCont> detalle = repoCont.findByIdEvento(ev.getIdEvento());
+
+    @RequestMapping(path = "/cotizacion", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_PDF_VALUE)
+    public ResponseEntity<ByteArrayResource> getCotizacion(@RequestBody Eventos ev) throws Exception {
+        Eventos evento;
+        try {
+            evento = repo.findById(new EventosId(ev.getIdEvento(), ev.getCajaZ())).get();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.noContent().eTag("Combinación evento: " + ev.getIdEvento() + " y cajaZ: " + ev.getCajaZ() + " no válida.").build();
+        }
+
         List<EvCont> byIdEventoArtiName = repoCont.findByIdEventoArtiName(ev.getIdEvento());
         EvMedios pie = medioRepository.findByIdEventoWithMedioName(ev.getIdEvento());
 
@@ -51,33 +60,18 @@ public class ReporteCotizacion {
         cotizacion.setDetalle(byIdEventoArtiName);
         cotizacion.setPie(pie);
         byte[] pdf = createPDF(cotizacion);
-        System.out.println(evento);
-        detalle.forEach(c-> System.out.println(c.toString()));
+        ResponseEntity<ByteArrayResource> response = ResponseEntity.ok().header("Content-Disposition", "attachment; filename=taa.pdf").body(new ByteArrayResource(pdf));
 
-        byIdEventoArtiName.forEach(c-> System.out.println("con nombre"+c.toString()));
-
-        System.out.println("PIE"+pie);
-
-        return ResponseEntity.ok().body(new ByteArrayResource(pdf));
+        return response;
     }
 
-
-    private byte[] createPDF(Cotizacion cotizacion) throws  Exception{
-
-
-
+    private byte[] createPDF(Cotizacion cotizacion) throws Exception {
         InputStream inputStream = getClass()
                 .getClassLoader().getResourceAsStream(fileName);
+        HashMap hm = new HashMap();
+        hm.put("model", cotizacion);
+        JasperPrint print = JasperFillManager.fillReport(inputStream, hm, new JRBeanCollectionDataSource(cotizacion.getDetalle()));
 
-        HashMap hm= new HashMap();
-        hm.put("model",cotizacion);
-        System.out.println(inputStream);
-        JasperPrint print=JasperFillManager.fillReport(inputStream,hm,new JRBeanCollectionDataSource(cotizacion.getDetalle()));
-
-        byte[] bytes = JasperExportManager.exportReportToPdf(print);
-
-
-        return bytes;
-
+        return JasperExportManager.exportReportToPdf(print);
     }
 }
