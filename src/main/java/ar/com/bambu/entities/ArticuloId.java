a941cf81463a804d84519255f56b188f110bf7a6
Sebas <sebaze@gmail.comasdf>
2020-04-23 12:27:56 -0300

    SZ adding equals and hash to articuloID

--- src/main/java/ar/com/bambu/entities/ArticuloId.java
+++ src/main/java/ar/com/bambu/entities/ArticuloId.java
@@ -1,6 +1,7 @@
 package ar.com.bambu.entities;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 public class ArticuloId implements Serializable {
 
@@ -21,5 +22,19 @@ public class ArticuloId implements Serializable {
 
     public void setCodigoBarra(String codigoBarra) {
         this.codigoBarra = codigoBarra;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ArticuloId)) return false;
+        ArticuloId that = (ArticuloId) o;
+        return codInterno == that.codInterno &&
+                Objects.equals(codigoBarra, that.codigoBarra);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(codInterno, codigoBarra);
     }
 }
