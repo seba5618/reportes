07c484fa4195a6144032b4c52b975bffa5a946b8
Sebas <sebaze@gmail.comasdf>
2020-04-22 22:54:32 -0300

    SZ - Adding logback configuration.

--- src/main/java/ar/com/bambu/ReporterApplication.java
+++ src/main/java/ar/com/bambu/ReporterApplication.java
@@ -1,22 +1,20 @@
 package ar.com.bambu;
 
-import ar.com.bambu.repos.EventosRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+
+
 
 @SpringBootApplication
 public class ReporterApplication {
 
-
-
+	private final static Logger LOGGER = LoggerFactory.getLogger(ReporterApplication.class);
 
 	public static void main(String[] args) {
+		LOGGER.info("Socotrecu version 1.0.1.");
 		SpringApplication.run(ReporterApplication.class, args);
-
-
-
 	}
 
 }
